aa31c31b4fc0e6325fdbd6372f040fcfc88b0f87
sharon mcdermott <sharonmcdermott@me.com>
2017-07-16 17:49:16 -0400

    Added assets of images to display. Had already created code to display images by the user shaking the device.

---
+++ CherrydaleBuds/CBViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  CherrydaleBuds
+//
+//  Created by sharon mcdermott on 7/15/17.
+//  Copyright © 2017 Sharon McDermott. All rights reserved.
+//
+
+import UIKit
+
+/// Main view controller with image view.
+class CBViewController: UIViewController {
+    
+    // MARK: - IBOutlet Variables -
+    
+    ///
+    @IBOutlet var imageView: UIImageView!
+    
+    // MARK: - Private Variables -
+    
+    ///
+    fileprivate var currentImageIndex = 0
+    
+    ///
+    fileprivate lazy var images = ["Image1", "Image2", "Image3"]
+    
+    // MARK: - UIViewController Methods -
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let image = UIImage (named: images[0]) {
+            imageView.image = image
+        }
+    }
+    
+    override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
+        //doShake()
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        switchImage()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - ViewController Methods -
+    
+    ///
+    func switchImage() {
+        currentImageIndex = (currentImageIndex + 1) % images.count
+        let image = UIImage(named: images[currentImageIndex])
+        imageView.image = image
+    }
+
+
+}
+
