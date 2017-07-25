057fe092cdfa4dbcc539020ac3d4254ae324a239
sharon mcdermott <sharonmcdermott@me.com>
2017-07-25 17:23:38 -0400

    Added flower images to the Assets and added the images to the images array.

--- CherrydaleBuds/CBViewController.swift
+++ CherrydaleBuds/CBViewController.swift
@@ -22,7 +22,7 @@ class CBViewController: UIViewController {
     fileprivate var currentImageIndex = 0
     
     /// These are the images in the array in a lazy closure to prevent it from being used more than once.
-    fileprivate lazy var images = ["Image1", "Image2", "Image3", "Image4", "Image5"]
+    fileprivate lazy var images = ["Image1", "Image2", "Image3", "Image4", "Image5", "Image6", "Image7", "Image8", "Image9", "Image10", "Image11", "Image12", "Image13", "Image14", "Image15", "Image16", "Image17", "Image18", "Image19", "Image20", "Image21", "Image22", "Image23", "Image24", "Image25", "Image26", "Image27", "Image28", "Image29", "Image30", "Image31", "Image32", "Image33", "Image34", "Image35", "Image36", "Image37", "Image38", "Image39", "Image40"]
 
     
     // MARK: - UIViewController Methods -
